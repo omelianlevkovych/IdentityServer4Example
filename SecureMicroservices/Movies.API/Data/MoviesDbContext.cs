8f9e2a4799501f95c01b28d8f6d1c4a9fd168642
OmelianLevkovych <oml@e-boks.dk>
2021-06-24 13:09:08 +0300

    (draft) Initial solution

---
+++ SecureMicroservices/Movies.API/Data/MoviesDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.API.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.API.Data
+{
+    public class MoviesDbContext : DbContext
+    {
+        public MoviesDbContext(DbContextOptions<MoviesDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
